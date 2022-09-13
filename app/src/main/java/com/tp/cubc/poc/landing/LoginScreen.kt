43dedf9aab5e5272fb17f13cc522e886542f207f
Oster.Huang <oster.huang@gramltd.com>
2022-09-14 00:40:33 +0800

    重構LoginScreen且讓Preview不再綁定viewModel(google建議）

--- app/src/main/java/com/tp/cubc/poc/landing/LoginScreen.kt
+++ app/src/main/java/com/tp/cubc/poc/landing/LoginScreen.kt
@@ -1,8 +1,5 @@
 package com.tp.cubc.poc.landing
 
-import android.app.Application
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -10,35 +7,22 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.mbanking.cubc.login.repository.dataModel.LoginRequestBody
 import com.tp.cubc.poc.R
 import com.tp.cubc.poc.app.CubcAppViewModel
-import com.tp.cubc.poc.landing.repository.LandingApi
-import com.tp.cubc.poc.landing.repository.LandingRemoteDataSource
-import com.tp.cubc.poc.landing.repository.dataModel.AccessTokenResponse
-import com.tp.cubc.poc.landing.repository.dataModel.LoginResponseBodyResult
 import com.tp.cubc.poc.ui.bg.LoginBg
-import com.tp.cubc.poc.ui.bg.TreeBg
 import com.tp.cubc.poc.ui.theme.CubcAppTheme
-import com.tp.cubc.poc.ui.theme.Green500
-import com.tp.cubc.poc.util.http.HttpRequestBody
-import com.tp.cubc.poc.util.http.HttpResponseBody
 import kotlinx.coroutines.launch
-import retrofit2.Response
 
 @Composable
 fun LoginScreen(
@@ -60,6 +44,31 @@ fun LoginScreen(
         }
     }
 
+    PureLoginScreen(
+        loginViewModel.username,
+        loginViewModel.usermima,
+        onChangeUsername = {  loginViewModel.username = it },
+        onChangeUsermima = { loginViewModel.usermima = it },
+        onLogin = onLogin,
+        goRegister = goRegister,
+        goApplyMobileBank = goApplyMobileBank
+    )
+
+}
+
+/**
+ * ViewModel無法在Preview中初始化，所以將需要viewModel的元件拆離
+ */
+@Composable
+fun PureLoginScreen(
+    username: String,
+    usermima: String,
+    onChangeUsername: (String) -> Unit,
+    onChangeUsermima: (String) -> Unit,
+    onLogin: () -> Unit,
+    goRegister: () -> Unit,
+    goApplyMobileBank: () -> Unit
+) {
     LoginBg {
         Column(
             Modifier
@@ -69,135 +78,165 @@ fun LoginScreen(
         ) {
             Image(
                 modifier = Modifier
-                    .padding(0.dp, 100.dp, 0.dp, 60.dp)
+                    .padding(0.dp, 80.dp, 0.dp, 60.dp)
                     .wrapContentSize(),
                 painter = painterResource(R.drawable.ic_splash_logo),
                 contentDescription = "background_image",
                 contentScale = ContentScale.FillBounds
             )
 
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = MaterialTheme.colors.surface,
-                        shape = RoundedCornerShape(8.dp)
-                    ),
-                value = loginViewModel.username,
-                onValueChange = { loginViewModel.username = it },
-                placeholder = { Text("Please Enter the Username") }
+            LoginInputArea(
+                username,
+                usermima,
+                onChangeUsername,
+                onChangeUsermima,
+                onLogin
             )
-            Spacer(modifier = Modifier.height(50.dp))
-
-            OutlinedTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(
-                        color = MaterialTheme.colors.surface,
-                        shape = RoundedCornerShape(8.dp)
-                    ),
-                value = loginViewModel.usermima,
-                onValueChange = { loginViewModel.usermima = it },
-                placeholder = { Text("Please Enter the Password") }
-            )
-            Spacer(modifier = Modifier.height(40.dp))
-
-            Button(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(48.dp),
-                onClick = onLogin
-            ) {
-                Text("Login")
-            }
             Spacer(modifier = Modifier.height(18.dp))
 
-            Column(
-                Modifier
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                TextButton(onClick = goApplyMobileBank) {
-                    Text(
-                        stringResource(id = R.string.apply_mobile_bank),
-                        color = MaterialTheme.colors.onPrimary
-                    )
-                }
-                Row(
-                    Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Divider(
-                        modifier = Modifier.weight(0.5f),
-                        color = MaterialTheme.colors.onPrimary
-                    )
-                    Text(
-                        " or ",
-                        modifier = Modifier.weight(0.2f),
-                        textAlign = TextAlign.Center,
-                        color = MaterialTheme.colors.onPrimary
-                    )
-                    Divider(
-                        modifier = Modifier.weight(0.5f),
-                        color = MaterialTheme.colors.onPrimary
-                    )
-                }
-                TextButton(onClick = goRegister) {
-                    Text(
-                        stringResource(id = R.string.go_register),
-                        color = MaterialTheme.colors.onPrimary
-                    )
-                }
-            }
-
+            LoginRegisterActionArea(
+                goRegister = goRegister,
+                goApplyMobileBank = goApplyMobileBank
+            )
             Spacer(modifier = Modifier.weight(1.0f))
 
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(24.dp, 12.dp),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_login_location),
-                    contentDescription = "Location Icon",
-                    tint = MaterialTheme.colors.onPrimary
-                )
+            LoginOtherArea()
+            Spacer(modifier = Modifier.height(6.dp))
 
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_login_contact),
-                    contentDescription = "Location Icon",
-                    tint = MaterialTheme.colors.onPrimary
-                )
-
-                Icon(
-                    modifier = Modifier
-                        .height(44.dp)
-                        .width(88.dp),
-                    painter = painterResource(id = R.drawable.ic_login_language_khmer),
-                    contentDescription = "Location Icon",
-                    tint = MaterialTheme.colors.onPrimary
-                )
-            }
+            Text("V 0.1.1", color = MaterialTheme.colors.onPrimary)
         }
     }
 }
 
+@Composable
+fun LoginInputArea(
+    username: String,
+    usermima: String,
+    onChangeUsername: (String) -> Unit,
+    onChangeUsermima: (String) -> Unit,
+    onLogin: () -> Unit,
+) {
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colors.surface,
+                shape = RoundedCornerShape(8.dp)
+            ),
+        value = username,
+        onValueChange = onChangeUsername,
+        placeholder = { Text("Please Enter the Username") }
+    )
+    Spacer(modifier = Modifier.height(50.dp))
+
+    OutlinedTextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = MaterialTheme.colors.surface,
+                shape = RoundedCornerShape(8.dp)
+            ),
+        value = usermima,
+        onValueChange = onChangeUsermima,
+        placeholder = { Text("Please Enter the Password") }
+    )
+    Spacer(modifier = Modifier.height(40.dp))
+
+    Button(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        onClick = onLogin
+    ) {
+        Text("Login")
+    }
+}
+
+@Composable
+fun LoginRegisterActionArea(
+    goRegister: () -> Unit,
+    goApplyMobileBank: () -> Unit
+) {
+    Column(
+        Modifier
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        TextButton(onClick = goApplyMobileBank) {
+            Text(
+                stringResource(id = R.string.apply_mobile_bank),
+                color = MaterialTheme.colors.onPrimary
+            )
+        }
+        Row(
+            Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Divider(
+                modifier = Modifier.weight(0.5f),
+                color = MaterialTheme.colors.onPrimary
+            )
+            Text(
+                " or ",
+                modifier = Modifier.weight(0.2f),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colors.onPrimary
+            )
+            Divider(
+                modifier = Modifier.weight(0.5f),
+                color = MaterialTheme.colors.onPrimary
+            )
+        }
+        TextButton(onClick = goRegister) {
+            Text(
+                stringResource(id = R.string.go_register),
+                color = MaterialTheme.colors.onPrimary
+            )
+        }
+    }
+}
+
+@Composable
+fun LoginOtherArea() {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(24.dp, 12.dp),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_login_location),
+            contentDescription = "Location Icon",
+            tint = MaterialTheme.colors.onPrimary
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_login_contact),
+            contentDescription = "Location Icon",
+            tint = MaterialTheme.colors.onPrimary
+        )
+
+        Icon(
+            modifier = Modifier
+                .height(44.dp)
+                .width(88.dp),
+            painter = painterResource(id = R.drawable.ic_login_language_khmer),
+            contentDescription = "Location Icon",
+            tint = MaterialTheme.colors.onPrimary
+        )
+    }
+}
 
 @Preview(name = "phone", device = "spec:shape=Normal,width=375,height=790,unit=dp,dpi=480")
 @Composable
 fun PreviewScreen() {
     CubcAppTheme() {
-        val viewModel = LoginViewModel(LandingRemoteDataSource(object: LandingApi{
-            override suspend fun accessToken(requestBody: HttpRequestBody): Response<HttpResponseBody<AccessTokenResponse>> {
-                return Response.success(HttpResponseBody("", "", AccessTokenResponse("")))
-            }
-            override suspend fun login(loginRequest: LoginRequestBody): Response<HttpResponseBody<LoginResponseBodyResult>> {
-                return Response.success(HttpResponseBody("", "", LoginResponseBodyResult("")))
-            }
-
-        }))
-        LoginScreen(viewModel, {}, {}, {})
+        PureLoginScreen(
+            username = "",
+            "",
+            {},
+            {},
+            {}, {}, {})
     }
 }
